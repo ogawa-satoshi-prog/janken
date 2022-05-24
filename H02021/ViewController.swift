eedc70e2e661dc69ea84b44424349a05ee869aac
ash <ogawa.sato01@gmail.com>
2022-05-24 22:35:17 +0900

    Initial Commit

---
+++ H02021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H02021
+//
+//  Created by 小川慧 on 2022/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
